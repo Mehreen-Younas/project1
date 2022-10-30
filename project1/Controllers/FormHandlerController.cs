1cd83d4f8567fe7b59c76016216ede388926f4a4
Mehreenbutt1947@gmail.com <Mehreenbutt1947@gmail.com>
2022-10-30 21:35:28 +0500

    done

---
+++ project1/Controllers/FormHandlerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project1.Models;
+using project1.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace Forkful.Controllers
+{
+    [Route("[controller]")]
+    public class FormHandlerController : Controller
+    {
+        JsonPastaFile PastaService;
+       public FormHandlerController(JsonPastaFile pastaService)
+        {
+          this.PastaService = pastaService;
+        }
+
+        [HttpGet]
+
+        public string Get(int id, string name, string image)
+        {
+            Pasta obj = new Pasta();
+            obj.pasta_id = id;
+            obj.pasta_name = name;
+            obj.pasta_image = image;
+
+            IEnumerable<Pasta> pastasData = PastaService.getPastaData();
+            List<Pasta> listofPastas= pastasData.ToList();
+            listofPastas.Add(obj);
+
+           return JsonSerializer.Serialize<List<Pasta>>(listofPastas);
+        }
+
+    }
+}
